2295a385c1025b0221621301c608941ee9a78f99
caicindy87 <caicindy87@gmail.com>
2020-04-26 11:43:20 -0700

    Done.

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,6 +1,28 @@
 class BirdsController < ApplicationController
   def index
-    @birds = Bird.all
-    render json: @birds
+    birds = Bird.all
+    render json: birds, only: [:id, :name, :species]
+  end
+
+  def show
+    bird = Bird.find_by(id: params[:id])
+    # Also add an error message if bird record not found
+    # Option 1:
+    # if bird
+    #   render json: {
+    #   id: bird.id, 
+    #   name: bird.name, 
+    #   species: bird.species
+    #   }
+    # else
+    #   render json: { message: 'Bird not found' }
+    # end
+
+    # Option 2: slice method returns a new hash to be rendered
+    if bird
+      render json: bird.slice(:id, :name, :species)
+    else
+      render json: { message: 'Bird not found' }
+    end
   end
 end
